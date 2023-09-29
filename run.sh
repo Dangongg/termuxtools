2233f98a9bf81bfd64f6e53aec9415bb81d265d5
Dangongg <126990792+Dangongg@users.noreply.github.com>
2023-09-29 22:29:59 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,16 @@
+
+echo "                                               "
+echo " _             _     ____  _____ _____   _     "
+echo "| |_ ___ ___ _| |_ _|    \|  _  |   | |_| |_ _ "
+echo "|   | .'|   | . | | |  |  |     | | | | . | | | "
+echo "|_|_|__,|_|_|___|_  |____/|__|__|_|___|___|_  | "
+echo "                |___|                     |___| "
+echo ==============================================================
+echo handyDANdy loaded!
+echo Please select one of the options:
+echo ""(requires TermuxAPI)
+echo ""[1] sms-spam
+echo ""[2] torch-spam
+echo ""[3] exit
+echo ==============================================================
+wait
